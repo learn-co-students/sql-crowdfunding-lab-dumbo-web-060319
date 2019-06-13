bf4aaac53492797fbd59cf210e5102b4964246ab
TL <nbotapp1@gmail.com>
2019-06-13 16:30:22 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,42 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+	"SELECT title, sum(amount) FROM projects
+	INNER JOIN pledges ON projects.id = pledges.project_id
+	GROUP BY title
+	ORDER BY title ASC"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+	"SELECT name, age, sum(amount) FROM users
+	INNER JOIN pledges ON users.id = pledges.user_id
+	GROUP BY name
+	ORDER BY name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+	"SELECT title, (sum(pledges.amount) - funding_goal) FROM projects
+	INNER JOIN pledges ON projects.id = pledges.project_id
+	GROUP BY title
+	HAVING (sum(pledges.amount) - funding_goal) >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+	"SELECT name, sum(pledges.amount) AS pledge_amount FROM users
+	JOIN pledges ON users.id = pledges.user_id
+	GROUP BY name
+	ORDER BY pledge_amount ASC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+	"SELECT category, pledges.amount FROM projects
+	JOIN pledges on projects.id = pledges.project_id
+	WHERE category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+	"SELECT category, sum(pledges.amount) as pledge_amount FROM projects
+	JOIN pledges on projects.id = pledges.project_id
+	GROUP BY category
+	HAVING category = 'books'"
 end
